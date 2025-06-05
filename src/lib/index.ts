a624bfdf69fcc1ffa79b980ceabbfb00dffae9fd
ykcho <ykcho@solvitsystem.co.kr>
2025-06-05 23:11:34 +0900

    update lib index, module resolution and eslint config

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 // Reexport your entry components here
-import type {} from './dock-data'
-import {} from './DockLayout.svelte'
+export type {LayoutData} from './dock-data'
+export {default as DockLayout} from './DockLayout.svelte'
